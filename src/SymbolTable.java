69e5a5d06e53bcfcb60a86a0a90b7ec0eb5b8976
Mujtaba Ali <mujtaba15@outlook.com>
2026-05-11 19:54:08 +0500

    Added support for int, float and char Added lexeme, type and value to the symbol table

--- src/SymbolTable.java
+++ src/SymbolTable.java
@@ -2,18 +2,65 @@ import java.util.*;
 
 public class SymbolTable {
 
-    private HashMap<String, String> symTab;
+    public static class Symbol {
+        public String lexeme;
+        public String type;
+        public String value;
+
+        public Symbol(String lexeme, String type, String value) {
+            this.lexeme = lexeme;
+            this.type = type;
+            this.value = value;
+        }
+    }
+
+    private HashMap<String, Symbol> symTab;
 
     public SymbolTable() {
         symTab = new HashMap<>();
     }
 
+    public void put(String lexeme, String type, String value) {
+        symTab.put(lexeme, new Symbol(lexeme, type, value));
+    }
+
+    public Symbol getSymbol(String lexeme) {
+        return symTab.get(lexeme);
+    }
+
+    public String getType(String lexeme) {
+        Symbol sym = symTab.get(lexeme);
+        return sym != null ? sym.type : null;
+    }
+
+    public String getValue(String lexeme) {
+        Symbol sym = symTab.get(lexeme);
+        return sym != null ? sym.value : null;
+    }
+
+    public void setValue(String lexeme, String value) {
+        Symbol sym = symTab.get(lexeme);
+        if (sym != null) {
+            sym.value = value;
+        }
+    }
+
     public void put(String key, String typeAndName) {
-        symTab.put(key, typeAndName);
+
+        if (typeAndName.startsWith("int")) {
+            put(key, "int", "");
+        } else if (typeAndName.startsWith("float")) {
+            put(key, "float", "");
+        } else if (typeAndName.startsWith("string")) {
+            put(key, "string", "");
+        } else if (typeAndName.startsWith("char")) {
+            put(key, "char", "");
+        }
     }
 
     public String get(String key) {
-        return symTab.get(key);
+        Symbol sym = symTab.get(key);
+        return sym != null ? (sym.type + sym.lexeme) : null;
     }
 
     public boolean containsKey(String key) {
@@ -29,16 +76,34 @@ public class SymbolTable {
     }
 
     public void print() {
-        System.out.println("=".repeat(50));
-        System.out.println("SYMBOL TABLE");
-        System.out.println("=".repeat(50));
-        for (Map.Entry<String, String> entry : symTab.entrySet()) {
-            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        System.out.println("=".repeat(60) + "\n");
+        System.out.println(String.format("%-20s %-15s %s", "LEXEME", "TYPE", "VALUE"));
+        System.out.println("-".repeat(60));
+        for (Symbol sym : symTab.values()) {
+            System.out.println(String.format("%-20s %-15s %s", sym.lexeme, sym.type, sym.value));
         }
-        System.out.println("=".repeat(50));
     }
 
     public int size() {
         return symTab.size();
+    }
+
+    public Collection<Symbol> getAllSymbols() {
+        return symTab.values();
+    }
+
+    public String exportToString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("SYMBOL TABLE\n");
+        sb.append("=".repeat(60)).append("\n");
+        sb.append(String.format("%-20s %-15s %s\n", "LEXEME", "TYPE", "VALUE"));
+        sb.append("=".repeat(60)).append("\n");
+
+        for (Symbol sym : symTab.values()) {
+            sb.append(String.format("%-20s %-15s %s\n", sym.lexeme, sym.type, sym.value));
+        }
+
+        sb.append("=".repeat(60)).append("\n");
+        return sb.toString();
     }
 }
